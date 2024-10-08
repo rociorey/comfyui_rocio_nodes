a86986c59c3d9213ee5985443c9e6e331945a3d1
Rocio Rey <60386636+rociorey@users.noreply.github.com>
2024-10-08 10:29:33 +0100

    Update rocio_custom_nodes.py

--- rocio_custom_nodes.py
+++ rocio_custom_nodes.py
@@ -26,55 +26,6 @@ class RocioBranchNode:
         
 
 
-class RocioImageSizer:
-    def __init__(self):
-        pass
-
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "model_type": (["SD","SDXL"],),
-                "aspect_ratio_width": ("INT",{
-                    "default": 1,
-                    "step":1,
-                    "display": "number"
-                }),
-                "aspect_ratio_height": ("INT",{
-                    "default": 1,
-                    "step":1,
-                    "display": "number"
-                })
-            }
-        }
-
-    RETURN_TYPES = ("INT","INT")
-    RETURN_NAMES = ("Width", "Height")
-
-    FUNCTION = "run"
-
-    CATEGORY = "CodyCustom"
-
-    def run(self, model_type, aspect_ratio_width, aspect_ratio_height):
-        # Define the total pixel counts for SD and SDXL
-        total_pixels = {
-            'SD': 512 * 512,
-            'SDXL': 1024 * 1024
-        }
-        
-        # Calculate the number of total pixels based on model type
-        pixels = total_pixels.get(model_type, 0)
-        
-        # Calculate the aspect ratio decimal
-        aspect_ratio_decimal = aspect_ratio_width / aspect_ratio_height
-        
-        # Calculate width and height
-        width = math.sqrt(pixels * aspect_ratio_decimal)
-        height = pixels / width
-        
-        # Return the width and height as a tuple of integers
-        return (int(round(width)), int(round(height)))
-
 class RocioConditionalPrompt:
     @classmethod
     def INPUT_TYPES(cls):
@@ -104,29 +55,26 @@ class RocioResizeImage:
         return {
             "required": {
                 "images": ("IMAGE",),
-                "width": ("INT", {"default": 512, "min": 64, "max": 2048}),
-                "height": ("INT", {"default": 512, "min": 64, "max": 2048}),
+                "width": ("INT", {"forceInput": True}),
+                "height": ("INT", {"forceInput": True}),
             }
         }
 
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "process_resize"
 
-    CATEGORY = "HiFORCE/Image/Zoom"
 
     def process_resize(self, images, width, height):
         return (process_resize_image(images, width, height),)
     
 
 NODE_CLASS_MAPPINGS = {
-    "RocioImageSizer": RocioImageSizer,
     "RocioBranchNode": RocioBranchNode,
     "RocioConditionalPrompt": RocioConditionalPrompt,
     "RocioResizeImage": RocioResizeImage
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "RocioImageSizer": "Rocio Image Sizer",
     "RocioBranchNode": "Rocio Image Branch",
     "RocioConditionalPrompt": "Rocio Conditional Prompt",
     "RocioResizeImage": "Rocio Resize Image"
